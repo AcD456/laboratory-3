e75d7b6fe36b1f376047d398fc1ee05b6652a73c
Dmitri <dmitri.lifantev@gmail.com>
2024-12-13 19:38:20 +0300

    main menu

---
+++ main.py
@@ -0,0 +1,23 @@
+
+
+def main_menu():
+    while True:
+        print("\nВыберите, откуда взять даты на проверку:")
+        print("1. Ввод с клавиатуры.")
+        print("2. Из файла.")
+        print("3. Найти даты на сайте.")
+        print("0. Завершение работы программы.")
+
+        choice = input("Введите номер варианта: ")
+
+        if choice == "1":
+            pass
+        elif choice == "2":
+            pass
+        elif choice == "3":
+            pass
+        elif choice == "0":
+            print("Завершение работы программы.")
+            return 0
+
+main_menu()
